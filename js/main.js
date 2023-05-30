dfbeea1237e979fc7112e5df6f0c6b26605083ab
PabloBona <pablobonasera@gmail.com>
2023-05-30 18:29:29 -0300

    Update and restructuring main.js

--- js/main.js
+++ js/main.js
@@ -1,92 +1,82 @@
-//create an array of localStorage element, or an empty array if no localStorage
-let books = JSON.parse(localStorage.getItem('books')) || [];
-const bookList = document.querySelector('.book-List');
-const addBookForm = document.querySelector('.book-form');
-class Book {
-  //constructor and properties
-    constructor(title,author) {
-      // Instance Properties, describes de current state of that object
-      // this.*** is about the current object, the object wich es created by a class
-      this.title = title
-      this.author =  author
-  }
-  //here are the Class's methodes
-  static removeBook = (index) => {
-    books = books.filter((book, i) => i !== index);
-    saveBooksToLocalStorage(books);
+class Books {
+  constructor() {
+    // Instance Properties, describes de current state of that object
+    // this.*** is about the current object, the object wich es created by a class
+    this.books = [];
+    this.bookList = document.querySelector('.book-List');
+    this.addForm = document.querySelector('.book-form');
+
+    // Next are methods: By calling this function in the constructor, we ensure that
+    // books are automatically loaded and displayed when a new instance of the Books class created.
+    this.loadBooksFromLocalStorage();
+    this.addForm.addEventListener('submit', this.saveFormSubmit.bind(this));
   }
 
-  static addBook = (book) => {
-    books.push(book);
-    saveBooksToLocalStorage(books);
-    const newIndex = books.length - 1;
-    createBookElement(book, newIndex);
+  // Instant Methods, they use instant Properties to achieve the results
+  createBookElement(book, index) {
+    const bookContainer = document.createElement('div');
+    const titleElement = document.createElement('p');
+    const authorElement = document.createElement('p');
+    const removeBtn = document.createElement('button');
+    const title = document.createTextNode(book.title);
+    const author = document.createTextNode(book.author);
+    const remove = document.createTextNode('Remove');
+
+    titleElement.appendChild(title);
+    authorElement.appendChild(author);
+    removeBtn.appendChild(remove);
+    bookContainer.appendChild(titleElement);
+    bookContainer.appendChild(authorElement);
+    bookContainer.appendChild(removeBtn);
+
+    removeBtn.addEventListener('click', () => {
+      this.removeBook(index);
+      bookContainer.remove();
+    });
+
+    this.bookList.appendChild(bookContainer);
   }
 
-}
+  renderBookList() {
+    this.books.forEach((book, index) => {
+      this.createBookElement(book, index);
+    });
+  }
 
-const saveBooksToLocalStorage = (books) => {
-  localStorage.setItem('books', JSON.stringify(books));
-}
+  saveBooksToLocalStorage() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
 
-const renderBookList = () => {
-  books.forEach((book, index) => {
-    createBookElement(book, index);
-  });
-}
+  loadBooksFromLocalStorage() {
+    const dataSaved = localStorage.getItem('books');
+    if (dataSaved) {
+      this.books = JSON.parse(dataSaved);
+      this.renderBookList();
+    }
+  }
 
-const loadBooksFromLocalStorage = () => {
-  const dataSaved = localStorage.getItem('books');
-  if (dataSaved) {
-    books = JSON.parse(dataSaved);
-    renderBookList();
+  addBook(book) {
+    this.books.push(book);
+    this.saveBooksToLocalStorage();
+    const newIndex = this.books.length - 1;
+    this.createBookElement(book, newIndex);
+  }
+
+  removeBook(index) {
+    this.books = this.books.filter((book, i) => i !== index);
+    this.saveBooksToLocalStorage();
+  }
+
+  saveFormSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(this.addForm);
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const newBook = { title, author };
+    this.addBook(newBook);
+    this.addForm.reset();
   }
 }
-
-//Instant Methods, they use instant Properties to achieve the results
-
-const saveFormSubmit = (e) => {
-  e.preventDefault();
-  const formData = new FormData(addBookForm);
-  const title = formData.get('title');
-  const author = formData.get('author');
-  const newBook = { title, author };
-  Book.addBook(newBook)
-  addBookForm.reset();
-}
-
-//A table that will contain all books
-const tableOfBooks = document.createElement('table')
-
-const createBookElement = (book, index) => {
-  //Create a tableRow as books list
-  const tableRow = document.createElement('tr');
-  const bookDetailsContainer = document.createElement('td');
-  const removeBtnContainer = document.createElement('td');
-  bookDetailsContainer.classList.add("book_details")
-  removeBtnContainer.classList.add("remove_btn_container")
-  //Create book's elements
-  const removeBtn = document.createElement('button')
-  removeBtn.appendChild(document.createTextNode("remove"))
-  const bookDetails = document.createTextNode(`"${book.title}" by ${book.author}`)
-  //append elment on their parents
-  bookDetailsContainer.appendChild(bookDetails)
-  removeBtnContainer.appendChild(removeBtn)
-  tableRow.appendChild(bookDetailsContainer);
-  tableRow.appendChild(removeBtnContainer)
-
-  removeBtn.addEventListener('click', () => {
-    Book.removeBook(index);
-    tableRow.remove();
-  });
-  tableOfBooks.appendChild(tableRow);
-  bookList.appendChild(tableOfBooks);
-}
-
-addBookForm.addEventListener('submit',saveFormSubmit)
-  window.onload = () => {
-    loadBooksFromLocalStorage()
-}
-
 
 // eslint-disable-next-line no-new
+new Books();
